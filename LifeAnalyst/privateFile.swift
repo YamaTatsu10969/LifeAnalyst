ff78c4d3d9cd99bb5acabf485c42b17cfc2df051
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-02-03 14:24:29 +0900

    feat:add_gitiginore

---
+++ LifeAnalyst/privateFile.swift
@@ -0,0 +1,15 @@
+//
+//  privateFile.swift
+//  LifeAnalyst
+//
+//  Created by 山本竜也 on 2019/2/3.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import Foundation
+
+class privateFile {
+    
+    static let GoogleAPI = "AIzaSyAdhVYNsgGH1nFPWdIHf5WiJ6WABbBPu1s"
+    
+}
