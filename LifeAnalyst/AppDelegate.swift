48c05df4efeb4caf511819d8d43e2053b19b2ba0
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-02-03 14:03:24 +0900

    gitignore

--- LifeAnalyst/AppDelegate.swift
+++ LifeAnalyst/AppDelegate.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    let GoogleAPI = "AIzaSyDqFYNG008D8zVaqQLCh2fk--zqdSeOAcc"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey(GoogleAPI)
         return true
     }
 
